1e5b4d6aee6ca704b9fe5a26e1406382acf73781
RinoaTakako <steff0109@gmail.com>
2025-01-24 16:13:46 +0000

    Second

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import random
 
+# import random library to generate random number 
+
 x = random.randint(1,10)
 print(x)
+
+# Ask the user for their input
 
 def get_user_guess():
   return int(input("Please input a number between 1 and 10: "))
   
+# Start and play game!
 
 def play_game():
   correct_number = x
@@ -17,11 +22,15 @@ def play_game():
     if user_input == correct_number:
   
       attempts += 1
-      print(f"Correct Guess! It took you {attempts} to get the correct number.")
+      print(f"Correct Guess! It took you {attempts} tries to get the correct number.")
       break
 
-    else:
+    elif user_input > correct_number:
       attempts += 1 
-      print(f"Incorrect guess! You have tried {attempts} number of guesses.")
+      print(f"Incorrect guess! You have tried {attempts} number of guesses. Try lower next time.")
+      
+    elif user_input < correct_number:
+      attempts +=1 
+      print(f"Incorrect guess! You have tried {attempts} number of guesses. Try higher next time.")
     
 play_game()
